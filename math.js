50d7432b335ed30b9c47ae282a6dc65267f171ca
miguelamezcua1 <amezcua_miguel01@student.smc.edu>
2025-07-15 02:27:20 -0700

    Add isOdd() function

--- math.js
+++ math.js
@@ -24,3 +24,13 @@ function isEven(n) {
 // Example test
 console.log(isEven(4)); // Should print true
 console.log(isEven(7)); // Should print false
+
+
+// Checks if a number is odd
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+
+// Example test
+console.log(isOdd(3)); // Should print true
+console.log(isOdd(8)); // Should print false
